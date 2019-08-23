64d9c632f1f1ae1d6392ac89ad108972ac223073
Tim Hatch <tim@timhatch.com>
2019-08-22 23:34:07 -0700

    Implementation of a concurrency-limited gather.
    This is the 80% support for gather, plus concurrency limiting.  While
    you can make thousands of coroutines that wait on a Semaphore, that
    would still spend time busy-waiting.
    
    This does not support cancellation.

--- aioitertools/asyncio.py
+++ aioitertools/asyncio.py
@@ -9,7 +9,7 @@ Provisional library.  Must be imported as `aioitertools.asyncio`.
 
 import asyncio
 import time
-from typing import Awaitable, Iterable, Optional, Set, Tuple, cast
+from typing import Any, Awaitable, Dict, Iterable, List, Optional, Set, Tuple, cast
 
 from .types import AsyncIterator, T
 
@@ -66,3 +66,66 @@ async def as_completed(
 
         for item in done:
             yield await item
+
+
+async def gather(
+    *args: Awaitable[T],
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+    return_exceptions: bool = False,
+    limit: int = -1
+) -> List[Any]:
+    """Like asyncio.gather but with a limit on concurrency.
+
+    Much of the complexity of gather comes with it support for cancel, which we
+    omit here.  Note that all results are buffered.
+    """
+
+    # For detecting input duplicates and reconciling them at the end
+    input_map: Dict[Awaitable[T], List[int]] = {}
+    # This is keyed on what we'll get back from asyncio.wait
+    pos: Dict[asyncio.Future[T], int] = {}
+    ret: List[Any] = [None] * len(args)
+
+    pending: Set[asyncio.Future[T]] = set()
+    done: Set[asyncio.Future[T]] = set()
+
+    next_arg = 0
+
+    while True:
+        while next_arg < len(args) and (limit == -1 or len(pending) < limit):
+            # We have to defer the creation of the Task as long as possible
+            # because once we do, it starts executing, regardless of what we
+            # have in the pending set.
+            if args[next_arg] in input_map:
+                input_map[args[next_arg]].append(next_arg)
+            else:
+                # We call ensure_future directly to ensure that we have a Task
+                # because the return value of asyncio.wait will be an implicit
+                # task otherwise, and we won't be able to know which input it
+                # corresponds to.
+                task: asyncio.Future[T] = asyncio.ensure_future(args[next_arg])
+                pending.add(task)
+                pos[task] = next_arg
+                input_map[args[next_arg]] = [next_arg]
+            next_arg += 1
+
+        # pending might be empty if the last items of args were dupes;
+        # asyncio.wait([]) will raise an exception.
+        if pending:
+            done, pending = await asyncio.wait(
+                pending, loop=loop, return_when=asyncio.FIRST_COMPLETED
+            )
+            for x in done:
+                if return_exceptions and x.exception():
+                    ret[pos[x]] = x.exception()
+                else:
+                    ret[pos[x]] = x.result()
+
+        if not pending and next_arg == len(args):
+            break
+
+    for lst in input_map.values():
+        for i in range(1, len(lst)):
+            ret[lst[i]] = ret[lst[0]]
+
+    return ret
