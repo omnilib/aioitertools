904ade143c05dbea6d494ba1e52e8d493a112ade
John Reese <john@noswap.com>
2020-11-08 19:54:59 -0800

    Initial attribution config/usage

--- aioitertools/__init__.py
+++ aioitertools/__init__.py
@@ -6,9 +6,8 @@ itertools and builtins for AsyncIO and mixed iterables
 """
 
 __author__ = "John Reese"
-__version__ = "0.7.0"
-
 from . import asyncio
+from .__version__ import __version__
 from .builtins import enumerate, iter, list, map, max, min, next, set, sum, zip
 from .itertools import (
     accumulate,
