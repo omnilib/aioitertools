0f722f42a41b3e800bd9de2119f1666bdf6802e0
Amethyst Reese <amy@n7.gg>
2022-09-16 21:47:58 -0700

    Make docs buildable without virtualenv, fix #113
    [ghstack-poisoned]

--- docs/conf.py
+++ docs/conf.py
@@ -4,20 +4,15 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 import datetime
+import pathlib
+import sys
+
+root = pathlib.Path(__file__).parent.parent
+print(f"root = {repr(root)}")
+sys.path.insert(0, root.as_posix())
 
 project = "aioitertools"
 copyright = f"{datetime.date.today().year}, Amethyst Reese"
